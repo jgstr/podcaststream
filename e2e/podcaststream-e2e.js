37a06e61a2e57f2d85663feef0932da875056611
Aaron <aaron@aarongoold.com>
2019-03-13 20:18:35 -0700

    Added node-docker-api module but so far cannot get it to work.

--- e2e/podcaststream-e2e.js
+++ e2e/podcaststream-e2e.js
@@ -1,9 +1,30 @@
+'use strict';
+const {Docker} = require('node-docker-api');
+
+const docker = new Docker({ socketPath: '/var/run/docker.sock' });
+
 describe("Podcaststream Broadcaster", function() {
 
+    // before(function() {
+    //
+    //     docker.container.create({
+    //         Image: 'podnode2',
+    //         name: 'e2e_test'
+    //     })
+    //         .then(container => container.start())
+    //         .then(container => container.stop())
+    //         .then(container => container.restart())
+    //         .then(container => container.delete({ force: true }))
+    //         .catch(error => console.log(error));
+    //
+    // });
+
     it("should return a running status", function(browser){
+
         browser
-            .url('http://localhost:9000/server-status')
+            .url('http://localhost:8000/server-status')
             .expect.element('#go-status').to.be.present;
+
     });
 
 });

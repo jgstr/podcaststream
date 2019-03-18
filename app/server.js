146d4c31f2fa1c45a4dfdbfef76eb6716c016c02
Aaron <aaron@aarongoold.com>
2019-03-18 12:39:22 -0700

    FALLBACK POINT: The docker-compose and/or Dockerfiles for their respective services do not connect node to mysql. BUT the server.js app with run just using "node app/server.js" while the MySQL database runs in a docker container DO connect to each other via the localhost of my laptop.

--- app/server.js
+++ app/server.js
@@ -6,12 +6,38 @@ const express = require('express');
 const PORT = 9000;
 const HOST = '0.0.0.0';
 
+// Connect to MySQL Database
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    port     : '3306',
+    user     : 'root',
+    password : 'root',
+    database : 'broadcast'
+});
+
+connection.connect();
+
+var result;
+
+connection.query('SELECT status FROM broadcaster WHERE id=1 LIMIT 1', function (error, results, fields) {
+
+    if (error) {
+        throw error;
+    }
+
+    result = results[0].status;
+    console.log('The status is: ', results[0].status);
+});
+
+connection.end();
+
 // App
 const app = express();
 app.get('/server-status', (req, res) => {
 
     // Now the database needs to deliver this message to "trigger"  #go-status
-    res.send('<div id=go-status>Broadcasting</div>\n');
+    res.send(`<div id=go-status>${result}</div>\n`);
 });
 
 app.listen(PORT, HOST);

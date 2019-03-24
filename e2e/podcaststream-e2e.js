dca095177ea73439e71632244a8a413197d2f7dd
Aaron <aaron@aarongoold.com>
2019-03-24 14:03:34 -0700

    Fallback point: When running docker-compose from terminal (ie, not using the npm docker-compose module), podcaststream-e2e.js passes the test.

--- e2e/podcaststream-e2e.js
+++ e2e/podcaststream-e2e.js
@@ -1,28 +1,27 @@
 'use strict';
-const {Docker} = require('node-docker-api');
+const path = require('path');
+const compose = require('docker-compose');
 
-const docker = new Docker({ socketPath: '/var/run/docker.sock' });
 
 describe("Podcaststream Broadcaster", function() {
 
     // before(function() {
     //
-    //     docker.container.create({
-    //         Image: 'podnode2',
-    //         name: 'e2e_test'
-    //     })
-    //         .then(container => container.start())
-    //         .then(container => container.stop())
-    //         .then(container => container.restart())
-    //         .then(container => container.delete({ force: true }))
-    //         .catch(error => console.log(error));
-    //
+    //     compose.upAll({ cwd: path.join(__dirname, '..'), log: true })
+    //         .then(
+    //             () => { console.log('Docker-compose ran.')},
+    //             err => { console.log('Something went wrong:', err.message)}
+    //         );
+
+
     // });
+
+    console.log("   >> Pretend e2e.js launched docker-compose.")
 
     it("should return a running status", function(browser){
 
         browser
-            .url('http://localhost:8000/server-status')
+            .url('http://localhost:9000/server-status')
             .expect.element('#go-status').to.be.present;
 
     });

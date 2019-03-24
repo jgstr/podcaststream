fb7156616ad285b28881f5c9a4252cf05511832d
Aaron <aaron@aarongoold.com>
2019-03-24 15:03:46 -0700

    walking skeleton test in podcaststream-e2e.js runs too soon. Can't figure out how to delay the it() test. Have tried using setTimeout() in various positions as well as this.timeout.

--- e2e/podcaststream-e2e.js
+++ e2e/podcaststream-e2e.js
@@ -5,25 +5,30 @@ const compose = require('docker-compose');
 
 describe("Podcaststream Broadcaster", function() {
 
-    // before(function() {
-    //
-    //     compose.upAll({ cwd: path.join(__dirname, '..'), log: true })
-    //         .then(
-    //             () => { console.log('Docker-compose ran.')},
-    //             err => { console.log('Something went wrong:', err.message)}
-    //         );
+    // Sets the suite test to timeout after 15 seconds
+    this.timeout(15000);
 
+    before(function() {
 
-    // });
+        compose.upAll({ cwd: path.join(__dirname, '..'), log: true })
+            .then(
+                () => { console.log('Docker-compose ran.')},
+                err => { console.log('Something went wrong:', err.message)}
+            );
+    });
 
-    console.log("   >> Pretend e2e.js launched docker-compose.")
+    after(function () {
+        compose.down().then(() => console.log('Docker-compose stopped and removed containers.'),
+            err => {console.log('Something went wrong when trying to stop containers:', err.message)});
+    })
 
-    it("should return a running status", function(browser){
+    // TODO: This test runs too soon. Figure out how to delay running it.
+    it("should return a running status", function (browser) {
 
         browser
             .url('http://localhost:9000/server-status')
             .expect.element('#go-status').to.be.present;
 
-    });
+    })
 
 });

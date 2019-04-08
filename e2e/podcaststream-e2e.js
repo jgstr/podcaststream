7f447a19dffb1644aa3590aaa934109f40309b8d
Aaron <aaron@aarongoold.com>
2019-04-08 14:16:38 -0700

    Nothing important committed. I only tinkered with `done()` and timer settings.  No progress.

--- e2e/podcaststream-e2e.js
+++ e2e/podcaststream-e2e.js
@@ -5,16 +5,14 @@ const compose = require('docker-compose');
 
 describe("Podcaststream Broadcaster", function() {
 
-    // Sets the suite test to timeout after 15 seconds
-    this.timeout(25000);
+    // Sets the suite test to timeout after 'n' milliseconds.
+    this.timeout(30000);
 
-    // Note: if these functions work like "normal" Javascript functions, the will get executed right away and not
-    // wait for the docker containers to initialize. Must use either the setTimeout or Promise.
-    before(function() {
+    before("Run docker-compose up", function() {
 
         compose.upAll({ cwd: path.join(__dirname, '..'), log: true })
             .then(
-                () => { console.log('Docker-compose ran.')},
+                () => { console.log('Docker-compose ran.') },
                 err => { console.log('Something went wrong:', err.message)}
             );
     });
@@ -22,14 +20,19 @@ describe("Podcaststream Broadcaster", function() {
     // TODO: This test runs too soon. Figure out how to delay running it. See note above before();
     it("should return a running status", function (browser) {
 
+        // The .waitForElementPresent() method is the polling feature from Nightwatch.
         browser
+            .pause(6000)
             .url('http://localhost:9000/server-status')
-            .expect.element('#go-status').to.be.present;
+            .waitForElementPresent('#go-status', 12000, 5000)
+            .expect.element('#go-status').to.be.present
+            .end();
+
+
 
     });
 
-    // Now this seems to be running too soon.
-    after(function () {
+    after("Run docker-composer down", function () {
         compose.down().then(() => console.log('Docker-compose stopped and removed containers.'),
             err => {console.log('Something went wrong when trying to stop containers:', err.message)});
     })

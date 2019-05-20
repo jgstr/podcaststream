d40fa175ffb66e3c6ec937de64193b97018fac2d
Aaron <aaron@aarongoold.com>
2019-05-20 11:23:16 -0700

    Started writing test for top-streams on web-app

---
+++ web-app/tests/top-streams.js
@@ -0,0 +1,10 @@
+import expect from "chai";
+
+describe("List of Top Streams", () => {
+    it("should display a list of tops streams.", () => {
+        // given, when, then (assertion).
+        // get React called, get DOM result, check the DOM.
+        // need fake browser (JS DOM).
+
+    })
+});

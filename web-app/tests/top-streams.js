a5ae150a73ff026d774ec3c774939986ef1c7bd9
Aaron <aaron@aarongoold.com>
2019-07-09 08:05:50 -0700

    Reverted to before react-scripts

--- web-app/tests/top-streams.js
+++ web-app/tests/top-streams.js
@@ -6,5 +6,7 @@ describe("List of Top Streams", () => {
         // get React called, get DOM result, check the DOM.
         // need fake browser (JS DOM).
 
+        // How to write tests with CreateReact App?? Look it up on their website, probably via Jest.
+
     })
 });

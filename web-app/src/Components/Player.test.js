9e08f37cbdcd03206214afaed9d0fe94751688c7
Aaron <aaron@aarongoold.com>
2019-09-11 09:17:08 -0700

    Added test for Player render conditional, null stream

--- web-app/src/Components/Player.test.js
+++ web-app/src/Components/Player.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {configure, shallow} from 'enzyme';
+import {configure, shallow, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({adapter: new Adapter()});
 
@@ -12,5 +12,9 @@ it('should renders an Object with stream values', () => {
 
     const player = shallow(<Player playerStream={{name: 'Name 1', length: '2200'}}/>);
     expectTextInElements(player, '.streamProperty', ['Name 1', '2200']);
+})
+
+it('should render without crashing when stream is empty', () => {
+    mount(<Player /> );
 })
 

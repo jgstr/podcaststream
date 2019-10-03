45a464fcddf6e05b9663ccc007a7f8442585620f
Aaron <aaron@aarongoold.com>
2019-10-03 16:14:29 -0700

    Moved data-store-it to the mocha test folder. Got docker-compose to launch a container, however async issues cause the test to run too early. Have not yet created the code to connect to the database. That should solve the async issues once complete.

--- app/data-store-it.js
+++
@@ -1,8 +0,0 @@
-// 1. Get MYSQL server running. Can use Docker image (directly).
-// 2. Make sure that the server is actually running. (accepting connections).
-//    Try connecting to it every so often until connected.
-// 3. Create data-store with some configuration that tells data-store where
-//    MySQL is (ie, the host/port/username/pass etc.)
-// 4. Use the data-store function and getting correct results.
-// -  Problems: no functions exist yet. Every read needs to have a corresponding
-//    write ***. Write something using adapter then read something using adapter.
---
+++ app/test-database/sql-scripts/insert-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO broadcaster (url)
+VALUES
+    ('http://broadcast-server:9001/broadcast-server-status');

5cf1abe6a0319d2a39f2d3a0c1abfc32cd9318c3
Aaron <aaron@aarongoold.com>
2019-10-16 16:56:56 -0700

    Got docker-compose to run but now have async problems with the mocha test as well as a database launching problem

--- app/test/data-store-it.js
+++ app/test/data-store-it.js
@@ -1,40 +1,43 @@
-// Integration Test:
 const path = require('path');
 const compose = require('docker-compose/index');
 const mysql = require('mysql');
-import {expect} from 'chai';
+const chai = require('chai');
+const expect = chai.expect;
 
 describe("Data Store", function () {
 
     before(function () {
 
-        // Debugging
-        console.log("From Data-store-it: " + path.join(__dirname, "..", "/app/test-database/"));
-
-        compose.upAll({cwd: path.join(__dirname, "..", "/app/test-database/"), log: true})
+        compose.upAll({cwd: path.join(__dirname, "..", "/test-database/"), log: true})
             .then(
                 () => {
 
                     // Wrap this in a timeout() at first, then attempt with retry().
-                    const pool = mysql.createPool({
-                        host: process.env.DATABASE_HOST || 'broadcaststream_db_1',
-                        port: 3306,
-                        user: 'root',
-                        password: 'root',
-                        database: 'broadcast'
-                    });
+                    setTimeout(() => {
+
+                        const pool = mysql.createPool({
+                            host: process.env.DATABASE_HOST || 'broadcaststream_db_1',
+                            port: 3306,
+                            user: 'root',
+                            password: 'root',
+                            database: 'broadcast'
+                        });
+
+                        console.log("*** Pool created. ***");
+
+                    }, 5000);
 
                     return console.log("Docker-compose ran.");
                 },
                 err => {
-                    console.log("Problems with Docker-compose: ", err.message)
+                    return console.log("Problems with Docker-compose: ", err.message)
                 }
             );
     });
 
     // TODO - Challenge: get the adapter result from above Promise into the it() test below.
     // Note:
-    // - No functions exist yet.
+    // - No functions in data-store exist yet.
     // - Every READ needs to have a corresponding WRITE
     // - Write something using ADAPTER then read something using ADAPTER.
 

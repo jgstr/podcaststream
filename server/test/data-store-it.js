ca8dca1633c464590658470adf45a05c3b4ae731
Aaron <aaron@aarongoold.com>
2019-11-03 15:15:17 -0600

    Refactored data-store-it test for more practical queries. Might need to make the query process its own helper function next.

--- server/test/data-store-it.js
+++ server/test/data-store-it.js
@@ -43,8 +43,14 @@ describe("Data Store", function () {
 
     it("should return a broadcast URL from the database", function () {
         const dataStore = createDataStore(pool);
-        dataStore.saveBroadcastURL('http://broadcast-server:9001/broadcast-server-status-2');
-        const getBroadcastQuery = "SELECT * FROM broadcaster WHERE id=2 LIMIT 1";
+        const broadcastURL = 'http://broadcast-server:9001/broadcast-server-status-2';
+
+        let queryTemplate = "SELECT * FROM broadcaster WHERE url=?";
+        let broadcastURLInsert = [broadcastURL];
+        let query = mysql.format(queryTemplate, broadcastURLInsert);
+
+        dataStore.saveBroadcastURL(broadcastURL);
+        const getBroadcastQuery = query;
         return dataStore.getBroadcastURL(getBroadcastQuery).then( (broadcastUrl) => {
             return expect(broadcastUrl).to.equal('http://broadcast-server:9001/broadcast-server-status-2');
         });
